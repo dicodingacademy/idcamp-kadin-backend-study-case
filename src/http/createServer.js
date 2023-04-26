7839a389f1bbf9e05384bad8b7045faf060d1336
Dimas Saputra <dimas.maulanads@gmail.com>
2023-04-26 13:35:43 +0700

    interpret response with pre response middleware

--- src/http/createServer.js
+++ src/http/createServer.js
@@ -1,11 +1,33 @@
 const Hapi = require('@hapi/hapi');
 const config = require('../utils/config');
+const ClientError = require('../exceptions/ClientError');
 
 async function createServer() {
-  return Hapi.server({
+  const server = Hapi.server({
     host: config.application.host,
     port: config.application.port,
   });
+
+  // interpret response with pre response middleware
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        return h.response({
+          status: 'fail',
+          message: response.message,
+          data: {},
+        }).code(response.statusCode);
+      }
+
+      return h.continue;
+    }
+
+    return h.continue;
+  });
+
+  return server;
 }
 
 module.exports = createServer;
