bfbb14110dea7a68d25c3632353f7ec52aa373b9
Dimas Saputra <dimas.maulanads@gmail.com>
2023-04-27 12:54:53 +0700

    implement refresh authentication

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -25,6 +25,24 @@ class AuthenticationsHandler {
       },
     }).code(201);
   }
+
+  async putAuthenticationHandler(request) {
+    const { refreshToken } = this._validator.validatePutAuthenticationPayload(request.payload);
+
+    await this._authenticationsService.verifyRefreshToken(refreshToken);
+
+    const { id } = this._jwtTokenize.verifyRefreshToken(refreshToken);
+
+    const accessToken = this._jwtTokenize.generateAccessToken({ id });
+
+    return {
+      status: 'success',
+      message: 'authentication berhasil diperbarui',
+      data: {
+        accessToken,
+      },
+    };
+  }
 }
 
 module.exports = AuthenticationsHandler;
